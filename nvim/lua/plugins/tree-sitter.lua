61d4710306824fe26f6cca4f598f79f3ed56704b
Irohayang <sybaodz@gmail.com>
2025-04-11 21:17:40 +0700

    Update tree-sitter.lua

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,1 +1,29 @@
-return { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+-- return { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			-- auto install
+			auto_install = true,
+			-- add language you want to highlight in code
+			ensure_installed = {
+				"lua",
+				"vim",
+				"javascript",
+				"typescript",
+				"tsx",
+				"go",
+				"java",
+				"json",
+	--			"zig",
+ 		           	"python",
+				"markdown",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
